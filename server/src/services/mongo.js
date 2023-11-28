68d3338882ab1e87acea701b1b05112f43b3435b
Johndiddles <johnadepelumi@gmail.com>
2023-11-28 12:04:02 +0100

    updating yaml file

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const MONGO_URL = process.env.MONGODB_URI;
+const MONGO_URL = process.env.MONGO_URL;
 
 mongoose.connection.once("open", () => {
   console.log("MongoDB connection is ready!");
